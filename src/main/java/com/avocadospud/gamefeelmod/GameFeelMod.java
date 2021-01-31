afb3cbb8b495d40fc66fffae57cc2385fd3542de
AvocadoSpud <69398022+AvocadoSpud@users.noreply.github.com>
2021-01-31 12:46:56 -0500

    Update GameFeelMod.java

--- src/main/java/com/avocadospud/gamefeelmod/GameFeelMod.java
+++ src/main/java/com/avocadospud/gamefeelmod/GameFeelMod.java
@@ -54,7 +54,7 @@ public class GameFeelMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("gamefeelmod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
